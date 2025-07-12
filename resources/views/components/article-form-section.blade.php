74e43065b26e92d632ab9c4ec61a64a5483ac9ff
Raul Diamantino <rauldiamantino25@gmail.com>
2025-07-11 23:46:28 -0300

    fix: review padding

--- resources/views/components/article-form-section.blade.php
+++ resources/views/components/article-form-section.blade.php
@@ -2,12 +2,12 @@
 
 <div {{ $attributes->merge(['class' => 'mx-auto w-full md:max-w-3xl bg-white rounded-lg']) }}>
     <form wire:submit="{{ $submit }}" class="space-y-10">
-        <div class="space-y-8">
+        <div class="p-4 space-y-8">
             {{ $form }}
         </div>
 
         @if (isset($actions))
-            <div class="flex justify-end space-x-4 pt-6 border-t border-gray-300">
+            <div class="flex justify-end space-x-4 py-6 px-4 border-t border-gray-300">
                 {{ $actions }}
             </div>
         @endif
--- resources/views/livewire/dashboard/articles/index.blade.php
+++ resources/views/livewire/dashboard/articles/index.blade.php
@@ -12,23 +12,17 @@
 <div>
     <div class="overflow-x-auto max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
         <table class="min-w-full text-sm text-left text-gray-700 bg-white border border-gray-300 rounded-lg overflow-hidden">
-            <thead class="text-xs text-gray-500 uppercase bg-gray-50">
-                <tr>
-                    <th class="py-3 border-b border-gray-300">Title</th>
-                    <th class="py-3 border-b border-gray-300">Status</th>
-                </tr>
-            </thead>
             <tbody>
                 @foreach ($articles as $article)
-                    <tr class="hover:bg-gray-50 border-b border-gray-300">
-                        <td class="py-4 bg-white rounded shadow hover:bg-gray-50">
-                            <h2 class="font-semibold">
+                    <tr class="hover:bg-gray-200 border-b border-gray-300">
+                        <td class="px-3 py-4">
+                            <h2>
                                 <a href="{{ route('dashboard.articles.edit', $article->article_number) }}" wire:navigate>
                                     {{ $article->title }}
                                 </a>
                             </h2>
                         </td>
-                        <td class="py-4">
+                        <td class="px-3 py-4">
                             @if ($article->status === 'published')
                                 <span
                                     class="inline-flex items-center px-2 py-0.5 text-xs font-semibold text-green-700 bg-green-100 rounded">
